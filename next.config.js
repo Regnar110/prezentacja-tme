4661955c4ebd39a07663439ac7fed73757b2fed7
Mateusz <66961646+Regnar110@users.noreply.github.com>
2025-02-16 12:54:51 +0100

    Cache control set

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    staleTimes: {
-      dynamic: 0, // client router cache - domyślny czas cachowania to 0
-      static: 180, // client router cache - domyślny czas cachowania to 300
-    },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=60",
+          },
+        ],
+      },
+    ]
   },
+  // experimental: {
+  //   staleTimes: {
+  //     dynamic: 0, // client router cache - domyślny czas cachowania to 0
+  //     static: 180, // client router cache - domyślny czas cachowania to 300
+  //   },
+  // },
   // logging: {
   //   fetches: {
   //     fullUrl: true,

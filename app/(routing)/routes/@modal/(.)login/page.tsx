86b3ad6be4094a38661998ead2248b3360fb9bdd
Bartosz <b.krol@tme.pl>
2025-07-04 15:20:02 +0200

    Add loginForm

--- app/(routing)/routes/@modal/(.)login/page.tsx
+++ app/(routing)/routes/@modal/(.)login/page.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback } from "react";
 import { useRouter } from "next/navigation";
+import { LoginForm } from '@/components/ui/loginForm';
 import './loginModal.css';
 
 export default function LoginModal() {
@@ -16,7 +17,7 @@ export default function LoginModal() {
 
     return (
         <dialog className={'dialog'} ref={dialogRef} onClose={() => router.back()}>
-            {'Ten route został przechwycony i pokazany w modalu'}
+            <LoginForm />
         </dialog>
     )
 }
